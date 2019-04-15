53f195ae3c5b6939c1f3abedade25ecbc160922d
Pawel Spychalski (DzikuVx) <pspychalski@gmail.com>
2019-04-15 12:57:52 +0200

    Heading

--- oled_display.cpp
+++ oled_display.cpp
@@ -67,15 +67,36 @@ void OledDisplay::renderPageDistance() {
         _display->drawString(0, 10, "ID: " + String(beacon->getId(), HEX));
         _display->drawString(70, 10, "RSSI: " + String(beacon->getRssi()));
 
+        int lastContact = (millis() - beacon->getLastContactMillis()) / 1000;
+        String contactString = "";
+
+        if (lastContact > 60) {
+            contactString = String(lastContact / 60, 0) + "min";
+        } else {
+            contactString = String(lastContact) + "s";
+        }
+
+        _display->drawString(70, 20, "T: -" + contactString);
+
         if (beacon->hasPos() && gps.satellites.value() > 5) {
             _display->setFont(ArialMT_Plain_16);
 
             double dst = TinyGPSPlus::distanceBetween(gps.location.lat(), gps.location.lng(), beacon->getLat(), beacon->getLon());
-            _display->drawString(0, 30, "Dst: " + String(dst, 0) + "m");
+            _display->drawString(0, 34, "Dst: " + String(dst, 0) + "m");
+
+            double courseTo =
+                TinyGPSPlus::courseTo(
+                    gps.location.lat(),
+                    gps.location.lng(),
+                    beacon->getLat(),
+                    beacon->getLon());
+
+            _display->setFont(ArialMT_Plain_10);
+            _display->drawString(00, 20, "Course: " + String(TinyGPSPlus::cardinal(courseTo)));
 
         } else {
             _display->setFont(ArialMT_Plain_16);
-            _display->drawString(0, 30, "No distance");
+            _display->drawString(0, 34, "No distance");
         }
 
         _display->setFont(ArialMT_Plain_10);
