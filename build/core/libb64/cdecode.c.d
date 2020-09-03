a28276fe6942281c2d3265e9dee5ff670e40d24d
Pawel Spychalski (DzikuVx) <pspychalski@gmail.com>
2020-09-03 15:18:26 +0200

    Drop build folder

--- build/core/libb64/cdecode.c.d
+++
@@ -1,7 +0,0 @@
-c:\Users\pspyc\Documents\Arduino\QmuBeaconLocator\build\core\libb64\cdecode.c.o: \
- C:\Users\pspyc\Documents\Arduino\hardware\espressif\esp32\cores\esp32\libb64\cdecode.c \
- C:\Users\pspyc\Documents\Arduino\hardware\espressif\esp32\cores\esp32\libb64\cdecode.h \
- C:\Users\pspyc\Documents\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/stdint.h \
- C:\Users\pspyc\Documents\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/machine/_default_types.h \
- C:\Users\pspyc\Documents\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/sys/features.h \
- C:\Users\pspyc\Documents\Arduino\hardware\espressif\esp32/tools/sdk/include/newlib/sys/_intsup.h
--- build/core/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-c:\Users\pspyc\Documents\Arduino\QmuBeaconLocator\build\core\libb64\cencode.c.o: \
- C:\Users\pspyc\Documents\Arduino\hardware\espressif\esp32\cores\esp32\libb64\cencode.c \
- C:\Users\pspyc\Documents\Arduino\hardware\espressif\esp32\cores\esp32\libb64\cencode.h
